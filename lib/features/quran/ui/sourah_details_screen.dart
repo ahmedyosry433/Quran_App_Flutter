1c01b2b02e790b711bf8bb85a5e0649e345fbd68
Ahmed Elesawy <a7med.elesawy0155066@gmail.com>
2024-04-29 20:42:52 +0300

    show all aya inline

--- lib/features/quran/ui/sourah_details_screen.dart
+++ lib/features/quran/ui/sourah_details_screen.dart
@@ -1,4 +1,9 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:quran_app/core/theme/colors.dart';
+import 'package:quran_app/core/theme/style.dart';
+import 'package:quran_app/core/widgets/my_appbar.dart';
 import 'package:quran_app/features/quran/data/model/quran_model.dart';
 
 class SurahDetailsScreen extends StatelessWidget {
@@ -7,16 +12,44 @@ class SurahDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: buildSourahList(),
+      backgroundColor: ColorsManager.lighterprimary,
+      body: Padding(
+        padding:
+            EdgeInsets.only(top: 40.h, right: 10.w, left: 10.w, bottom: 10.h),
+        child: SingleChildScrollView(
+          child: buildSourahList(),
+        ),
+      ),
     );
   }
 
   Widget buildSourahList() {
-    return ListView.builder(
-      itemCount: surah.verses.length,
-      itemBuilder: (context, index) {
-        return Text('${surah.verses[index].text}');
-      },
+    return Column(
+      children: [
+        Image.asset(
+          'assets/image/basmala.png',
+          height: 40.h,
+        ),
+        RichText(
+          textAlign: TextAlign.justify,
+          overflow: TextOverflow.visible,
+          text: TextSpan(
+            style: TextStyle(
+              fontSize: 15.sp,
+              color: Colors.black,
+            ),
+            children: surah.verses
+                .map(
+                  (e) => TextSpan(
+                    text: '${e.text} ${e.number} ',
+                    style: TextStyles.font16BlackRegular
+                        .copyWith(fontFamily: 'Quran'),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+      ],
     );
   }
 }
